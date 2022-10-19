9c85a7cdd7769ada2c0e1cdc6ed36da2a9e55b0e
sozi <sozyarnadir@gmail.com>
2022-10-19 22:48:08 +0300

    Assignment Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,43 @@
 import React from "react";
 
 function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
+    return ( <
+        nav >
+        <
+        a href = "#home" > I 'm a link!</a> <
+        /nav>
+    );
 }
 
 function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
+    return ( <
+        div id = "home" >
+        <
+        h1 > Home < /h1> <
+        /div>
+    );
 }
 
-{/* write an <About> component here */}
-
+function About() {
+    return ( <
+        div id = "about" >
+        <
+        h2 > About < /h2> <
+        /div>
+    );
+}
 
 function App() {
-  return (
-    <div>
-      <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
-    </div>
-  );
+    return ( <
+        div >
+        <
+        NavBar / > { /* add the <Home> component here */ } { /* write an <About> component here */ } <
+        Home / >
+        <
+        About / >
+        <
+        /div>
+    );
 }
 
 export default App;
